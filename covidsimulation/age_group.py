11f7e517043b30403c8b45ffeae79aa5df340e1e
Lucas Cardozo <lucas.cardozo@nexadigital.com.br>
2020-04-08 19:45:36 -0300

    fix typo and delete merge conflict garbage

--- covidsimulation/age_group.py
+++ covidsimulation/age_group.py
@@ -1,12 +1,9 @@
 
 class AgeGroup:
-  def __init__(self, index, severity, isolation_adherence, isolation_effectiveness, diagnosis_delay=None):
+  def __init__(self, index, severity, isolation_adherence, isolation_effectiveness, 
+               diagnosis_delay=None):
     self.index = index
     self.severity = severity
     self.isolation_adherence = isolation_adherence
     self.isolation_effectiveness = isolation_effectiveness
-<<<<<<< HEAD
-    self.diagnosis_delay = diagnosis_dealy  # If set, lab is ignored
-=======
-    self.diagnosis_delay = diagnosis_delay # If set, lab is ignored
->>>>>>> ec13295727caa9175eec144eab219b7fc186fe77
+    self.diagnosis_delay = diagnosis_delay  # If set, lab is ignored
