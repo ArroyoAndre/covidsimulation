f5ecc5d6d8e14f01e290fa1a78a74334f45fd240
Lucas Cardozo <lucas.cardozo@nexadigital.com.br>
2020-04-08 15:13:29 -0300

    initial work on translating the code to english

--- covidsimulation/age_group.py
+++ covidsimulation/age_group.py
@@ -1,8 +1,8 @@
 
 class AgeGroup:
-  def __init__(self, index, severity, isolation_adherence, isolation_effectiveness):
+  def __init__(self, index, severity, isolation_adherence, isolation_effectiveness, diagnosis_delay=None):
     self.index = index
     self.severity = severity
     self.isolation_adherence = isolation_adherence
     self.isolation_effectiveness = isolation_effectiveness
-    self.diagnosis_delay = diagnosis_delay  # if set, lab is ignored
+    self.diagnosis_delay = diagnosis_dealy  # If set, lab is ignored
